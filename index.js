14b9d588849aa5fdaa578cf231b02ea10ea422c6
Bhavna <bhavnavaishnav0402@gmail.com>
2024-06-18 15:23:09 +0530

    fifth commit

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    const newfruits = document.querySelector('.fruits');
+    const getTotalFruits = document.getElementById('Fruit-Count');
+
     const alt = () => {
         const evenFruit = document.querySelectorAll('.fruit:nth-child(even)');
-
-        for(let i=0; i<evenFruit.length; i++) {
-            evenFruit[i].style.backgroundColor = '#C2E78E';
-        }
+        evenFruit.forEach(fruit => fruit.style.backgroundColor = '#C2E78E');
+        
     }
-    
-
-
-const form = document.querySelector('form');
-const newfruits = document.querySelector('.fruits');
-const getTotalFruits = document.getElementById('Fruit-Count');
 
 const updateCount = () => {
     const fruitCount = document.querySelectorAll('.fruit').length; 
     getTotalFruits.textContent = `Total Fruits: ${fruitCount.toString().padStart(2, '0')}`;
-    alt();
+   
 }
 
 form.addEventListener('submit', function(event) {
@@ -24,23 +20,37 @@ form.addEventListener('submit', function(event) {
     const addFruits = document.getElementById('add');
     if(addFruits.value.trim()) {
         const newList = document.createElement('li');
-        newList.innerHTML = addFruits.value + '<button class="del">x</button>';
+        newList.innerHTML = addFruits.value + '<button class="del">x</button><button class="edit-btn">Edit</button>';
         newList.className = 'fruit';
+        
         newfruits.appendChild(newList);
         addFruits.value = '';
+        
     }
+    
     updateCount();
-    alt();
+   
 });
 
 newfruits.addEventListener('click', function(event) {
     if(event.target.classList.contains('del')) {
         const deleteFruit = event.target.parentElement;
         newfruits.removeChild(deleteFruit);
+        
+    } else if(event.target.classList.contains('edit-btn')) {
+        const editButton = event.target.parentElement;
+        
+        const newFruit = prompt("Edit fruit:", editButton.textContent.replace('xEdit', '').trim());
+            if (newFruit) {
+                editButton.firstChild.textContent = `${newFruit} `;
+                
+            }
+            
     }
     updateCount();
-    alt();
+    
 });
+
 updateCount();
 alt();
 });
